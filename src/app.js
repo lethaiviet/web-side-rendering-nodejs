26dd685598cba658686c1822a4feadeff54ff8e4
Viet Le <lethaiviet92@gmail.com>
2022-06-14 03:18:04 +0700

    + Add template engines

--- src/app.js
+++ src/app.js
@@ -6,13 +6,18 @@ import logger from 'morgan';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
+import { engine } from 'express-handlebars';
+
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, '/resources/views'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
