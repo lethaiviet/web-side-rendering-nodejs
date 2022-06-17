d5f55832acd1ff622ce22721bb66d045aed72c2e
Viet Le <lethaiviet92@gmail.com>
2022-06-17 07:13:45 +0700

    * using config timestamp of mongoose

--- src/models/Customers.js
+++ src/models/Customers.js
@@ -1,34 +1,31 @@
 import mongoose, { Schema } from 'mongoose'
 import mongoose_delete from 'mongoose-delete'
 
-const CustomerSchema = new Schema({
-    first_name: {
-        type: String,
-        minLength: [1, "'first_name' is too short!"],
-        maxlength: 10,
-        required: true,
+const CustomerSchema = new Schema(
+    {
+        first_name: {
+            type: String,
+            minLength: [1, "'first_name' is too short!"],
+            maxlength: 10,
+            required: true,
+        },
+        last_name: {
+            type: String,
+            minlength: [1, "'last_name' is too short!"],
+            maxlength: 10,
+            required: true,
+        },
+        age: {
+            type: Number,
+            min: [1, "'age' should be in [1, 130]"],
+            max: [130, "'age' should be in [1, 130]"],
+            required: true,
+        },
     },
-    last_name: {
-        type: String,
-        minlength: [1, "'last_name' is too short!"],
-        maxlength: 10,
-        required: true,
+    {
+        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
     },
-    age: {
-        type: Number,
-        min: [1, "'age' should be in [1, 130]"],
-        max: [130, "'age' should be in [1, 130]"],
-        required: true,
-    },
-    createAt: {
-        type: Date,
-        createAt: Date.now,
-    },
-    updateAt: {
-        type: Date,
-        updateAt: Date.now,
-    },
-})
+)
 
 CustomerSchema.plugin(mongoose_delete, {
     deletedAt: true,
